ec653fe432dab125f4110d0dc36c220fe99db335
santiak3 <71164211+santiak3@users.noreply.github.com>
2022-04-27 21:16:30 -0500

    Updated index.js to display the image and excerpt

--- Realtor/gatsby-config.js
+++ Realtor/gatsby-config.js
@@ -33,6 +33,7 @@ module.exports = {
     },
     `gatsby-transformer-remark`,
     `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
