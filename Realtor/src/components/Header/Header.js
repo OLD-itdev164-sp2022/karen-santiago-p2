96341124c80310ada995552cda187a061a5c92f3
santiak3 <71164211+santiak3@users.noreply.github.com>
2022-04-30 13:03:59 -0500

    Used the Icon component in Header.js

--- Realtor/src/components/Header/Header.js
+++ Realtor/src/components/Header/Header.js
@@ -2,8 +2,11 @@ import React from "react"
 import PropTypes from "prop-types"
 import { Link } from "gatsby"
 import styled from 'styled-components'
+import { Search } from 'styled-icons/feather'
 import { H1 } from '../Heading'
+import { IconButton } from '../Button'
 import { Section } from '../Section'
+
 
 const Outer = styled.header`
   background: ${props => props.theme.header.backgroundColor};
@@ -40,7 +43,7 @@ const Header = ({ siteTitle }) => (
             </H1>
           </Section>
             <Section width={1/12}>
-              Search
+              <IconButton icon={<Search />} />
             </Section>
         </Section>
     </Inner>
