25d73d09a78e52b9297b166a99686e1c074d52d9
santiak3 <71164211+santiak3@users.noreply.github.com>
2022-04-29 22:23:14 -0500

    Used Main and Footer components in Layout

--- Realtor/src/components/layout.js
+++ Realtor/src/components/layout.js
@@ -12,6 +12,8 @@ import styled, { ThemeProvider } from 'styled-components'
 import { Gray } from '../themes/Gray'
 import { Header } from "./Header/Header"
 import GlobalStyle from './GlobalStyle'
+import { Main } from './Main'
+import { Footer } from './Footer'
 
 const Content = styled.div`
   margin: 0 auto;
@@ -36,15 +38,12 @@ const Layout = ({ children }) => {
     <GlobalStyle />
       <Header siteTitle={data.site.siteMetadata.title || `Title`} />
       <Content>
-        <main>{children}</main>
-        <footer
-        style ={{
-          marginTop: `2rem`,
-        }}>
+        <Main>{children}</Main>
+        <Footer>
           © {new Date().getFullYear()}, Built with
           {` `}
           <a href="https://www.gatsbyjs.com">Gatsby</a>
-        </footer>
+        </Footer>
       </Content>
     </ThemeProvider>
   )
